6f1746f6c98f98ca573385821c2e7144345c0fdc
Marco Monteiro <kanxuk@gmail.com>
2025-10-31 11:04:21 +0000

    Updated Endpoint Url

--- src/EnglandCountyCouncilsNamesAPIClient.php
+++ src/EnglandCountyCouncilsNamesAPIClient.php
@@ -7,7 +7,9 @@ use Kanxpack\RapidAPIClient\RapidAPIClient;
 class EnglandCountyCouncilsNamesAPIClient {
 
 	private static $instance;
-	
+	protected static $rapidapiUrl = 'https://kanxpack-england-county-councils-names-list-api.p.rapidapi.com/Kanxpack/EnglandCountyCouncilsNamesAPIData/refs/tags/v1.0.2/data/all.json';
+	protected static $rapidapiHost = 'kanxpack-england-county-councils-names-list-api.p.rapidapi.com';
+
     public static function getInstance() : self
     { 
     	return empty(self::$instance) ? (new self()) : self::$instance; 
@@ -15,7 +17,7 @@ class EnglandCountyCouncilsNamesAPIClient {
 
 	public static function all($key) : \Kanxpack\RapidAPIClient\RapidAPIClient
 	{
-        return RapidAPIClient::setUrl('https://kanxpack-england-county-councils-names-list-api.p.rapidapi.com/all')->setHost('kanxpack-england-county-councils-names-list-api.p.rapidapi.com')->setKey($key)->get();
+        return RapidAPIClient::setUrl($this->rapidapiUrl)->setHost($this->rapidapiHost)->setKey($key)->get();
 	}
 
 }
